33163237d1a8d6a3cad863c3d9d4d0b65edbbd37
Odirlei Guilherme <118445178+OdirleiGuilherme80@users.noreply.github.com>
2024-05-17 16:50:03 -0300

    add folder CaixaEletronico

---
+++ controle-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class CaixaEletronico {
+    public static void main(String[] args) throws Exception {
+        //Saldo disponível
+        double saldo = 500.0;
+        
+        //objeto import Scanner e Locale
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        //informação ao usuário
+        System.out.println("Por favor digite o valor de retirada: ");
+        double valorSolicitado = scanner.nextDouble();
+
+        if (valorSolicitado < saldo){
+            double saldoRestante = (saldo - valorSolicitado);
+        
+            System.out.println("Valor solicitado para retirada: " + valorSolicitado);
+            System.out.println("saldo disponível em conta é de: " + saldoRestante);
+
+        }
+        else {
+            System.out.println("Saldo indisponível, faça a consulta para saber saldo disponível!");
+        }
+    }
+}
